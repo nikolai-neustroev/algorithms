5dc5422e8bc02bf0c36ade7570c657f98a7b4d5d
Nikolai Neustroev <nikolai.neustroev@outlook.com>
2021-11-07 09:02:06 +0900

    Check If N and Its Double Exist (Brute Force algorithm)

--- arrays/2_search/check_if_n_and_its_double_exist.py
+++ arrays/2_search/check_if_n_and_its_double_exist.py
@@ -1,8 +1,16 @@
 from typing import List
 
 
-def n_and_m_exist(arr: List[int]) -> bool:
-    pass
+def n_and_m_exist_(arr: List[int]) -> bool:
+    """Brute Force algorithm"""
+    checker = []
+    for i, val in enumerate(arr):
+        for j, val_ in enumerate(arr):
+            if (i != j) & (j < len(arr)) & (val == 2 * val_):
+                checker.append(True)
+            else:
+                checker.append(False)
+    return any(checker)
 
 
 if __name__ == '__main__':
@@ -10,5 +18,5 @@ if __name__ == '__main__':
     # and M such that N is the double of M (i.e. N = 2 * M).
     test = [10, 2, 5, 3]
     expected = True
-    actual = n_and_m_exist(test)
+    actual = n_and_m_exist_(test)
     assert actual == expected
