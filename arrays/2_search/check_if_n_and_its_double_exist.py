e71eb83a4b4427177279798c417675ea50661708
Nikolai Neustroev <nikolai.neustroev@outlook.com>
2021-11-07 09:13:52 +0900

    Check If N and Its Double Exist O(n)

--- arrays/2_search/check_if_n_and_its_double_exist.py
+++ arrays/2_search/check_if_n_and_its_double_exist.py
@@ -13,10 +13,20 @@ def n_and_m_exist_(arr: List[int]) -> bool:
     return any(checker)
 
 
+def n_and_m_exist(arr: List[int]) -> bool:
+    """O(N)"""
+    hashmap = {}
+    for el in arr:
+        if hashmap.get(el / 2, False) or hashmap.get(el * 2, False):
+            return True
+        hashmap[el] = hashmap.get(el, 0) + 1
+    return False
+
+
 if __name__ == '__main__':
     # Given an array arr of integers, check if there exists two integers N
     # and M such that N is the double of M (i.e. N = 2 * M).
     test = [10, 2, 5, 3]
     expected = True
-    actual = n_and_m_exist_(test)
+    actual = n_and_m_exist(test)
     assert actual == expected
